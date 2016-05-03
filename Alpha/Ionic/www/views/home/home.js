de4d31c6a8e6f5ba8e905c87c0cb63827fac546b
Albert Darmawan <darmawanalbert@gmail.com>
2016-05-03 21:13:48 +0700

    Adding some more

--- Alpha/Ionic/www/views/home/home.js
+++ Alpha/Ionic/www/views/home/home.js
@@ -6,6 +6,17 @@ angular.module('App').controller('homeController', function ($scope, $state,$cor
       Auth.logout();
       $location.path("/login");
   }
-
+  $scope.pollings = [
+      { title: 'Unity 5 for Gamification', description : 'This is a survey about Unity 5 for Gamification ',creator: 'Albert Darmawan'},
+      { title: 'Ionic for Mobile Development', id: 2 },
+      { title: 'Binus International Evaluation Form', id: 3 },
+      { title: 'Newbinusmaya Evaluation Form', id: 4 },
+      { title: '2016 Thesis Execution', id: 5 },
+      { title: 'Polite App Review', id: 6 }
+  ];
 }
 );
+
+
+
+.controller('PollingsCtrl', function($scope) {
