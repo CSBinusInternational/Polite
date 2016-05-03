65d4440dcb26af9ce9c738a5d70495ef580f9165
Ferdy Pratama <ferdyp24@outlook.com>
2016-05-03 21:19:22 +0700

    add dummy data

--- Alpha/Ionic/www/views/home/home.js
+++ Alpha/Ionic/www/views/home/home.js
@@ -7,12 +7,12 @@ angular.module('App').controller('homeController', function ($scope, $state,$cor
       $location.path("/login");
   }
   $scope.pollings = [
-      { title: 'Unity 5 for Gamification', description : 'This is a survey about Unity 5 for Gamification ',creator: 'Albert Darmawan'},
-      { title: 'Ionic for Mobile Development', id: 2 },
-      { title: 'Binus International Evaluation Form', id: 3 },
-      { title: 'Newbinusmaya Evaluation Form', id: 4 },
-      { title: '2016 Thesis Execution', id: 5 },
-      { title: 'Polite App Review', id: 6 }
+      { id: 1, title: 'Unity 5 for Gamification', description : 'This is a survey about Unity 5 for Gamification ',creator: 'Albert Darmawan'},
+      { id: 2, title: 'Ionic for Mobile Development', description: 'This is a survey about Ionic for Mobile Development', creator: 'Ieuan Ignatius' },
+      { id: 3, title: 'Binus International Evaluation Form', description: 'This is a survey about Binus International Evaluation Form', creator: 'Ferdy Pratama' },
+      { id: 4, title: 'Newbinusmaya Evaluation Form', description: 'This is survey about Newbinusmaya Evaluation Form', creator:'Mark' },
+      { id: 5, title: '2016 Thesis Execution', description: 'This is a survey about 2016 Thesis Execution', creator:'Thomas Dwinata' },
+      { id: 6, title: 'Polite App Review', description:'This is a survey about Polite App Review', creator:'Matthew' }
   ];
 }
 );
