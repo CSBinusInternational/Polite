af7075dc042dd73d5d137f7a42e9a54d93b1daa7
Ieuan Ignatius <Ieuan Ignatius>
2016-05-12 04:13:27 +0700

    searchbar

--- Alpha/Ionic/www/views/sidemenu/sidemenu.js
+++ Alpha/Ionic/www/views/sidemenu/sidemenu.js
@@ -1,11 +1,10 @@
 'Use Strict';
-angular.module('App').controller('sidemenuController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
+angular.module('App').controller('sidemenuController', function ($scope, $state,$cordovaOauth, $localStorage,$ionicModal, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
   var ref = new Firebase(FURL);
 
   $scope.logout = function () {
       Auth.logout();
       $state.go('app.home');
   }
-
 }
 );
