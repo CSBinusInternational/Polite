6a57fb195855ed44315ab063c7e262e5a8da9328
Albert Darmawan <darmawanalbert@gmail.com>
2016-06-30 09:57:24 +0700

    Finished Polling summary

--- Alpha/Ionic/www/views/mypollingsummary/mypollingsummary.js
+++ Alpha/Ionic/www/views/mypollingsummary/mypollingsummary.js
@@ -8,4 +8,43 @@ angular.module('App').controller('myPollingSummaryController', function ($scope,
   var indexNum = $scope.currentPollingKey;
   var indexString = indexNum.toString();
   $scope.currentPolling =  $firebaseObject(ref.child('pollings').child(indexString));
+  $scope.pollReference = ref.child('pollings').child(indexString);
+
+
+  $ionicModal.fromTemplateUrl('views/mypollingsummary/analysis.html', {
+    scope: $scope,
+    animation: 'slide-in-left'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function(){
+    $scope.modal.hide();
+  };
+
+  $scope.getTotalQuestions = function(questionlist) {
+    var sum = 0;
+    angular.forEach(questionlist, function(value,key) {
+      sum +=1;
+    });
+    return sum;
+  };
+
+  $scope.getDeadline = function(deadlinedate) {
+      var temp = new Date(deadlinedate);
+      return temp;
+  };
+
+  $scope.getTotalResponses = function(answerlist) {
+    var sum = 0;
+    angular.forEach(answerlist, function(value,key) {
+      sum +=1;
+    });
+    return sum;
+  }
+
 });
