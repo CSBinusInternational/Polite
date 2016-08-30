2993036582f4d5e185bfb4c00223d06a1557946b
Albert Darmawan <darmawanalbert@gmail.com>
2016-08-31 00:23:13 +0700

    Fixing data schema for analysis

--- Alpha/Ionic_v1.1/www/views/mypollingsummary/mypollingsummary.js
+++ Alpha/Ionic_v1.1/www/views/mypollingsummary/mypollingsummary.js
@@ -29,34 +29,50 @@ angular.module('App').controller('myPollingSummaryController', function ($scope,
 
   // Setting up answer array (2D, O(n^2)), each index represents a questions
   $scope.answerArray = [];
-  var totalque = $scope.getTotalQuestions();
-  console.log("Total que : " + totalque);
-  var totalres = $scope.getTotalResponses();
-  console.log("Total res : " + totalres);
-  for (var i=0;i<100;i++) {
-      $scope.answerArray[i] = new Array();
-      for (var j=0;j<100;j++) {
-          $scope.answerArray[i][j] = "This is a test";
-      }
-  }
-  console.log("Initialize Answer Array : " + $scope.answerArray[1][0]);
-
-  $scope.answersObj = $firebaseObject(ref.child('pollings').child(indexString).child('answers'));
-  $scope.answersObj.$loaded().then(function(){
-    var ctr = 0;
-    angular.forEach($scope.answersObj, function(value,key) {
-        var innerctr = 0;
-        console.log("Current value : " + value);
-        console.log("Current key : " + key);
-        angular.forEach(value.answerset, function(innervalue,innerkey) {
-            console.log("Current inner value : " + innervalue);
-            $scope.answerArray[innerctr][ctr] = innervalue;
-            innerctr+=1;
-        });
-        ctr+=1;
+  $scope.currentPolling.$loaded().then(function(data) {
+    var totalque = 0;
+    angular.forEach($scope.currentPolling.questions, function(value,key) {
+      totalque +=1;
     });
-    console.log("Answer Array : " + $scope.answerArray[1][0]);
+    console.log("Total que : " + totalque);
+    var totalres = 0;
+    angular.forEach($scope.currentPolling.answers, function(value,key) {
+      totalres +=1;
+    });
+    $scope.totalResponse = totalres;
+    $scope.totalQuestion = totalque;
+    console.log("Total response : " + $scope.totalResponse);
+    console.log("Total question : "+ $scope.totalQuestion);
+    var i=0;
+    var j=0;
+    for (i=0;i<$scope.totalQuestion;i++) {
+        $scope.answerArray[i] = new Array();
+        for (j=0;j<$scope.totalResponse;j++) {
+            $scope.answerArray[i][j] = "";
+        }
+    }
+    console.log("Initialize Answer Array : " + $scope.answerArray[1][0]);
+    $scope.answersObj = $firebaseObject(ref.child('pollings').child(indexString).child('answers'));
+    $scope.answersObj.$loaded().then(function(){
+      var ctr = 0;
+      angular.forEach($scope.answersObj, function(value,key) {
+          var innerctr = 0;
+          console.log("Current value : " + value);
+          console.log("Current key : " + key);
+          angular.forEach(value.answerset, function(innervalue,innerkey) {
+              console.log("Current inner value : " + innervalue);
+              $scope.answerArray[innerctr][ctr] = innervalue;
+              // [questions][response_number] -> each question type is a row
+              innerctr+=1;
+          });
+          ctr+=1;
+      });
+      console.log("Answer Array : " + $scope.answerArray[1][0]);
+    });
   });
+
+
+
 
   $ionicModal.fromTemplateUrl('views/mypollingsummary/analysis.html', {
     scope: $scope,
