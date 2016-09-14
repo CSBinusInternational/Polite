e2d5a00654d8b66376f65e386d91ad379932d153
Albert Darmawan <darmawanalbert@gmail.com>
2016-09-14 13:53:20 +0700

    Finishing graph, only putting the data left

--- Alpha/Ionic_v1.3.1/www/views/mypollingsummary/mypollingsummary.js
+++ Alpha/Ionic_v1.3.1/www/views/mypollingsummary/mypollingsummary.js
@@ -67,11 +67,19 @@ angular.module('App').controller('myPollingSummaryController', function ($scope,
           });
           ctr+=1;
       });
-      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
       $scope.series = ['Series A', 'Series B'];
+      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.labelsHorizontal = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
       $scope.dataaa = [
           [65, 59, 80, 81, 56, 55, 40],
           [28, 48, 40, 19, 86, 27, 90]
+      ];
+      $scope.dataHorizontal = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      $scope.dataPie = [
+        65, 59, 80, 81, 56, 55, 40
       ];
       //console.log("Answer Array : " + $scope.answerArray[1][0]);
     });
