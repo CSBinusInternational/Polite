8a5800c0edfecc1ed5a52544408e7106e51c8235
Albert Darmawan <darmawanalbert@gmail.com>
2016-09-19 22:20:55 +0700

    Modifying Search.js

--- Alpha/Ionic_v1.3.1/www/views/search/search.js
+++ Alpha/Ionic_v1.3.1/www/views/search/search.js
@@ -1,3 +1,4 @@
+//$scope.temp = {searchText:""};
 'Use Strict';
 angular.module('App').controller('searchController', function ($scope, $ionicModal, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, $firebaseArray, Auth, FURL, Utils) {
   console.log("Search Controller!");
@@ -20,12 +21,17 @@ angular.module('App').controller('searchController', function ($scope, $ionicMod
     $scope.selectedpolling = $firebaseObject(ref.child('pollings').child(chosenpollingkey));
     $scope.myquestionset = $firebaseArray(ref.child('pollings').child(chosenpollingkey).child('questions'));
     //$scope.questionref = ref.child('pollings').child(chosenpollingkey).child('questions');
+    $scope.mandatoryArray = new Array();
+    angular.forEach()
     $scope.myanswerset = [];
     $scope.myquestionset.$loaded()
     .then(function(data){
+        var mCounter = 0;
         angular.forEach(data, function(value, key) {
             console.log("Value Type : " + value.type);
             console.log("Key : " + key);
+            $scope.mandatoryArray[mCounter] = value.mandatory;
+            mCounter++;
             if (value.type=="radio") {
               $scope.tempradio = [];
               angular.forEach(value.type.choices, function(cvalue,ckey) {
@@ -42,7 +48,7 @@ angular.module('App').controller('searchController', function ($scope, $ionicMod
             }
             if (value.type=="range") {
               $scope.temprange = [];
-              angular.forEach(value.type.steps, function(cvalue,ckey) {
+              angular.forEach(value.type.choices, function(cvalue,ckey) {
                 $scope.temprange.push(false);
               });
               $scope.myanswerset.push($scope.temprange);
@@ -79,9 +85,9 @@ angular.module('App').controller('searchController', function ($scope, $ionicMod
     console.log($scope.submissionData);
     var isPopup = false;
     angular.forEach($scope.myanswerset, function(submitValue,submitKey) {
-        console.log("Key of each answer set : "+submitKey)
+        console.log("Key of each answer set : "+submitKey);
         console.log("Value of each answer set : " + submitValue);
-        if (submitValue==''||submitValue===null) {
+        if ((submitValue==''||submitValue===null)&&$scope.mandatoryArray[submitKey]) {
             isPopup = true;
         }
     });
